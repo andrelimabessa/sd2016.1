a20439a004a48af8da0f70a3410d45759202c184
Madjer B oliveira <madjerbo@gmail.com>
2016-02-18 20:37:38 -0300

    Subindo a classe Field

---
+++ Entities/Field.py
@@ -0,0 +1,17 @@
+class Field:
+	__player: ""
+	__field: []
+	__size: 0
+	def __init__(self, player, size):
+		self.__player = player
+		self.__size = size
+		self.__creatField(size)
+
+	def __creatField(self):
+		for x in xrange(0,self.__size-1):
+				self.__field[]
+			pass
+		
+
+	def getPlayer(self):
+		return self.__player;
