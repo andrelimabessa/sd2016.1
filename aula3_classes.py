d538cf0755023ef82faac9ff2cce5f1ff0d560ed
christian pompeu da silva <0911118>
2016-02-25 19:16:43 -0300

    teste

--- aula3_classes.py
+++ aula3_classes.py
@@ -1,11 +1,11 @@
+import random
+
 campo_de_batalha = [[0 for j in range(5)] for i in range(5)]
 
-def colocando_barcos(quant_barcos):
-    for i in range(quant_barcos):
-        for j in range(quant_barcos):
-            campo_de_batalha[i][j] = 1
-
-colocando_barcos(3)
-
-for i in range(5):
-    print(campo_de_batalha[i])
+for i in campo_de_batalha:
+    for j in i:
+        marca = random.randint(0,1)
+        print(marca)
+        if marca == 1:
+            i[j] = 1
+print(campo_de_batalha)
