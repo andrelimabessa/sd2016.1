10448440b8ce18bede8d159951fb20379d09d813
christian pompeu da silva <0911118>
2016-02-25 20:38:43 -0300

    inicio do programa

--- aula3_classes.py
+++ aula3_classes.py
@@ -3,9 +3,4 @@ import random
 campo_de_batalha = [[0 for j in range(5)] for i in range(5)]
 
 for i in campo_de_batalha:
-    for j in i:
-        marca = random.randint(0,1)
-        print(marca)
-        if marca == 1:
-            i[j] = 1
-print(campo_de_batalha)
+    print("Na linha")
