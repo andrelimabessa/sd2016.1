d79c2905c7e07380a0529061ac2b5bb3e3444407
christianpompeu <christianpompeu@gmail.com>
2016-02-27 13:43:15 -0300

    Estudando aulas anteriores

---
+++ aula2_laboratorio1/aula2_laboratorio1.py
@@ -0,0 +1,51 @@
+num = 13
+
+nome = "Nome"
+
+hello = "Hello"
+
+print(hello)
+
+print(hello + "World")
+
+print(hello * 3)
+
+print(hello[0])
+
+print(hello[-1])
+
+print(len(hello))
+
+print('e' in hello)
+
+var = "Faculdade 7 de Setembro"
+
+print(var[1:7])
+
+print(var[1:7:2])
+
+print(var[::-1])
+
+print(var.split())
+
+print(var.count("7"))
+
+var2 = var.split()
+
+print(var2)
+
+print("/".join(var2))
+
+tupla = ("Bom", "Dia")
+
+lista = ["Boa", "Noite", ["Boa", "Tarde"], 2]
+
+lista.append("texto")
+
+#tupla.append("texto")
+
+print(lista[1])
+
+a = list(range(5))
+
+print(a)
