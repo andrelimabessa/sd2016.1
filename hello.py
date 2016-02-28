cc43fb5c89a5ed5d7414602dfb69a551971fca63
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2016-02-28 20:06:45 -0300

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-from batlefield import Batle_Field
-        
-eixoX = input("Digite o posição x")
-eixoY = input("Digite o posição y")
-
-b = Batle_Field()
-print(b.jogar(eixoX, eixoY))
-    
-input()
