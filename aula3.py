1c3a9ac2e629ac3c8883446e732d511290ab3355
christianpompeu <christianpompeu@gmail.com>
2016-02-29 04:22:30 -0300

    Removi os arquivos que não serão usados

--- aula3.py
+++
@@ -1,6 +0,0 @@
-a = [1,2,3]
-
-try:
-    print(str(a[5]))
-except Exception as e:
-    print("Posição inexistente")
--- aula3_classes.py
+++
@@ -1,6 +0,0 @@
-import random
-
-campo_de_batalha = [[0 for j in range(5)] for i in range(5)]
-
-for i in campo_de_batalha:
-    print("Na linha")
