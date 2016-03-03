e7a1ad7eb90dab393ae7e09c43e398923498395f
victor escoto magalhaes <1120586>
2016-03-03 20:42:16 -0300

    Início da mudança de estrutura para cliente/servidor com thread

---
+++ batalha_naval_client.py
@@ -0,0 +1,18 @@
+class BatalhaNavalClient(object):
+    def __init__(self, ip_server):
+        
+
+    def mostraTabuleiro(self):
+        
+
+    def atirar(self, posicao):
+        
+
+    def novoJogo(self):
+        
+
+    def continuar(self):
+        
+
+teste = BatalhaNavalClient()
+        
