3db36a5eb2ace97969decb5f041b7e2f964910d0
christian pompeu da silva <0911118>
2016-03-10 20:31:09 -0300

    teste de import de classe

--- battleField/udp_cliente_battle_field.py
+++ battleField/udp_cliente_battle_field.py
@@ -1,18 +1,21 @@
 import socket
+import play_battle_field
 from datetime import datetime
 
 ENCODE = "UTF-8"
-HOST = '10.54.42.50'#'127.0.0.1'
+HOST = '127.0.0.1'#'10.54.42.50'#
 PORT = 5000
 MAX_BYTES = 65535
 
 def client():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	dest = (HOST, PORT)
-	text = input("Digite algum comando:\n")
-	data = text.encode(ENCODE)
-	sock.sendto(data, dest)
-	print(sock.getsockname())
-	data, address = sock.recvfrom(MAX_BYTES)
-	text = data.decode(ENCODE)
-	print(address, text)
+	while True:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		dest = (HOST, PORT)
+		#starting_game()
+		text = input("Digite algum comando:\n")
+		data = text.encode(ENCODE)
+		sock.sendto(data, dest)
+		print(sock.getsockname())
+		data, address = sock.recvfrom(MAX_BYTES)
+		text = data.decode(ENCODE)
+		print(address, text)
