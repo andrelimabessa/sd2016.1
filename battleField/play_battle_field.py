5c41783d0397eea1b169649924e962b39bb6de30
christianpompeu <christianpompeu@gmail.com>
2016-03-10 00:42:09 -0300

    inclusão de classe de comunicação com o servidor

--- battleField/play_battle_field.py
+++ battleField/play_battle_field.py
@@ -6,3 +6,23 @@ def starting_game():
 	print("1 - Ver estado do jogo.")
 	print("2 - Continuar jogo anterior.")
 	print("3 - Começar novo jogo.")
+
+	acao = input("Ação: ")
+
+	try:
+		if int(acao) == 1:
+			print("Você escolheu: Ver estado do jogo")
+			return 1
+		elif int(acao) == 2:
+			print("Você escolheu: Continuar jogo anterior.")
+			return 2
+		elif int(acao) == 3:
+			print("Você escolheu: Começar novo jogo.")
+			return 3
+		else:
+			raise Exception
+	except :
+		print("Precisa escolher uma opção válida.\n\n")
+		starting_game()
+
+starting_game()
