f943cafcc5d983e56f48aef4dc05c7d293e7a51d
christian pompeu da silva <0911118>
2016-03-10 21:04:08 -0300

    teste de conexao

--- battleField/play_battle_field.py
+++ battleField/play_battle_field.py
@@ -1,28 +1,34 @@
 import random
 import ast
 
-def starting_game():
-	print("O que deseja fazer?")
-	print("1 - Ver estado do jogo.")
-	print("2 - Continuar jogo anterior.")
-	print("3 - Começar novo jogo.")
+class game(object):
 
-	acao = input("Ação: ")
+	def starting_game(self):
+		print("O que deseja fazer?")
+		print("1 - Ver estado do jogo.")
+		print("2 - Continuar jogo anterior.")
+		print("3 - Começar novo jogo.")
 
-	try:
+		acao = input("Ação: ")
+
+		#try:
 		if int(acao) == 1:
 			print("Você escolheu: Ver estado do jogo")
-			return 1
+			print(acao)
+			return acao
 		elif int(acao) == 2:
 			print("Você escolheu: Continuar jogo anterior.")
-			return 2
+			print(acao)
+			return acao
 		elif int(acao) == 3:
 			print("Você escolheu: Começar novo jogo.")
-			return 3
+			print(acao)
+			return acao
 		else:
-			raise Exception
-	except :
-		print("Precisa escolher uma opção válida.\n\n")
-		starting_game()
+			print("Precisa escolher uma opção válida.\n\n")
+			#return starting_game()
+		#except :
+		#	print("Precisa escolher uma opção válida.\n\n")
+		#	starting_game()
 
 #starting_game()
--- battleField/udp_cliente_battle_field.py
+++ battleField/udp_cliente_battle_field.py
@@ -1,5 +1,5 @@
 import socket
-import play_battle_field
+from play_battle_field import game
 from datetime import datetime
 
 ENCODE = "UTF-8"
@@ -7,10 +7,27 @@ HOST = '127.0.0.1'#'10.54.42.50'#
 PORT = 5000
 MAX_BYTES = 65535
 
+game = game()
 def client():
 	while True:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		dest = (HOST, PORT)
+
+		option = True
+		#while option:
+		acao = game.starting_game()
+		if acao == 1:
+			print(acao)
+			option = False#break
+		elif acao ==2:
+			print(acao)
+			option = False#break
+		elif acao ==3:
+			print(acao)
+			option = False#break
+		else:
+			print(acao)
+		#print(game.starting_game())
 		text = input("Digite algum comando:\n")
 		data = text.encode(ENCODE)
 		sock.sendto(data, dest)
