1314ef4564ba835796232bd3a8c94a185ec38c89
madjer <madjerbo@gmail.com>
2016-04-14 20:32:52 -0300

    Revert "Merge remote-tracking branch 'origin/madjer'"
    This reverts commit 61dbae5527e5adf0b5ab6b72260ed970eb40c099, reversing
    changes made to 0b55cd926891ee778225b7f9ebf3ba20f4a7d74a.

--- Main.py
+++
@@ -1,6 +0,0 @@
-from Entities.FieldManager import FieldManager
-from Entities.Player import Player
-from Helper.GameManager import GameManager
-from Private.Server import Server
-
-gm = GameManager()
