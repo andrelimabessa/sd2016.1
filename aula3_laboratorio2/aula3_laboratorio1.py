46ea47830cbe1d5edc21d1b6e7e045c69117886d
christianpompeu <christianpompeu@gmail.com>
2016-06-02 06:43:09 -0400

    delecao de pastas obsoletas apos backup

--- aula3_laboratorio2/aula3_laboratorio1.py
+++
@@ -1,8 +0,0 @@
-a = [1,2,3]
-
-try:
-	print(str(a[5]))
-except (IndexError, TypeError):
-	print("Posição inexistente no array!")
-finally:
-	print("Fim do teste de impressão de posições")
--- batalha_Naval/protocolo_servidor.py
+++
@@ -1,6 +0,0 @@
-#protocolo_servidor.py
-
-class ProtocoloServidor(object):
-
-    def criar_tabuleiro(self, requisicao):
-        print("Informe a quantidade de colunas")
